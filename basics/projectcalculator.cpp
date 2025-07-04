5f527cb991b2ddfc7ba5c50112777e0bc29196bb
palak-dhiman-max <uk1211417@gmail.com>
2025-07-04 21:02:07 +0530

    basic

---
+++ basics/projectcalculator.cpp
@@ -0,0 +1,52 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"project calculator"<<endl;
+    int sum ;
+    int num;
+    char operation;
+    
+        cout<<"Enter your number"<<endl;
+        cin>>sum;
+    
+    while(true){
+        
+        cout<<"Enter your operator"<<endl;
+        cin>>operation;
+        
+        
+        if(operation=='='){
+            break;
+        }
+        
+        
+        
+        cout<<"Enter you next number"<<endl;
+        cin>>num;
+        switch(operation){
+            case'+':
+            sum=sum+num;
+            break;
+            case'-':
+            sum = sum- num;
+            break;
+            case'*':
+            sum = sum*num;
+            break;
+            case'/':
+            sum = sum/num;
+            break;
+            case'%':
+            
+            sum = sum% num;
+            break;
+            default:
+            cout<<"invalid"<<endl;
+            }
+        
+        }
+        cout<<"Your result is equal to "<<sum<<endl;
+
+
+        }
