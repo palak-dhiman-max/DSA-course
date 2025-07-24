2148215d0cafe9d886250c953b7b4725963af2bb
palak-dhiman-max <uk1211417@gmail.com>
2025-07-24 21:26:14 +0530

    file

--- object-oriented-programming/destructor.cpp
+++ object-oriented-programming/destructor.cpp
@@ -1,68 +1,68 @@
 #include <iostream>
 using namespace std;
 
-class customer{
+// class customer{
 
-    public:
-    string name;
-    int *balance;
+ 
+//     string name;
+//     int *balance;
+//    public:
+//     customer(string a,int bal){
+//     name = a;
+//     balance = new int;
+//     *balance = bal;
 
-    customer(string a,int bal){
-a = name;
-balance = new int;
-*balance = bal;
+// cout<<"constructor has been called"<<endl;
 
-cout<<"constructor has been called"<<endl;
+//     }
 
-    }
-
-    ~customer(){
-        delete balance;
-        cout<<"destructor has been called";
+//     ~customer(){
+//         delete balance;
+//         cout<<"destructor has been called";
       
-    }
+//     }
 
  
 
-};
+// };
 
-int main ()
-{
-customer a1("palak",2000);
+// int main ()
+// {
+// customer a1("palak",2000);
 
-}
+// }
 
 
 // order of destruction called is reverse as that of constructor called
 
-// class student{
+class student{
 
-//     public:
-//     string name;
-//     int * data;
+    public:
+    string name;
+    int * data;
 
-//     student(){
-//         name="4";
-//         cout<<"constructor has been called "<<name<<endl;
-//     }
+    student(){
+        name="4";
+        cout<<"constructor has been called "<<name<<endl;
+    }
 
-//     student(string name){
-// this->name = name;
-//  cout<<"constructor has been called"<<" "<<name<<endl;
-//     }
+    student(string name){
+this->name = name;
+ cout<<"constructor has been called"<<" "<<name<<endl;
+    }
 
-//     ~student(){
-//         delete data;
-//         cout<<"destructor has been called"<<" "<<name<<endl;
-//     }
+    ~student(){
+        delete data;
+        cout<<"destructor has been called"<<" "<<name<<endl;
+    }
 
-// };
+};
 
-// int main (){
-//     student s1("1"),s2("2"),s3("3");
-//     student *s4=new student;
-//  delete s4;
-// }
+int main (){
+    student s1("1"),s2("2"),s3("3");
+    student *s4 = new student;
+ delete s4;
+}
 
 
 
