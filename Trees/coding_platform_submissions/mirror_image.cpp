e78ae2d210b52ce495f69100c6bc4b96491ba912
palak-dhiman-max <uk1211417@gmail.com>
2026-06-09 18:58:34 +0530

    f

---
+++ Trees/coding_platform_submissions/mirror_image.cpp
@@ -0,0 +1,31 @@
+/*
+class Node {
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int x) {
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    void mirror(Node* root) {
+        // code here
+        if(!root){
+            return ;
+        }
+        
+        Node *temp = root->left;
+        root->left=root->right;
+        root->right = temp;
+        
+        mirror(root->left);
+        mirror(root->right);
+        
+    }
+};
