0367a57e99c70fafd0492b654b8e1da78a39099b
palak-dhiman-max <uk1211417@gmail.com>
2026-06-20 14:14:39 +0530

    f

---
+++ Trees/coding_platform_submissions/preorder_iterative.cpp
@@ -0,0 +1,39 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution {
+  public:
+    vector<int> preOrder(Node* root) {
+        // code here
+        stack <Node *>s;
+        vector <int>ans;
+        s.push(root);
+        
+        while(!s.empty()){
+            Node *temp = s.top();
+            s.pop();
+            ans.push_back(temp->data);
+            
+            if(temp->right){
+                s.push(temp->right);
+            }
+            
+               if(temp->left){
+                s.push(temp->left);
+            }
+            
+        }
+        
+        return ans;
+    }
+};
